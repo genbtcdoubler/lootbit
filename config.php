4449e889d6ee753f3e2963bfbeb35199939fb27f
genbtcdoubler <79389495+genbtcdoubler@users.noreply.github.com>
2021-03-07 11:27:54 -0800

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,5 @@
 <?php
-$UserAgent = "";
-AllCookie = "__cfduid="";
-_ga="";
-_gid="";_lrtk="";"
+$UserAgent = "Mozilla/5.0 (Linux; Android 6.0.1; Lenovo P1a42 Build/MMB29M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.91 Mobile Safari/537.36";
+$_luid = "1514235";
+$_lrtk = "QoBkA9pAZBsqRwWFeZEn02rLeTUKzfN8uXwW4L0lDvzLEqGjtOY0RmDw0Dnbwmrd";
 ?>
